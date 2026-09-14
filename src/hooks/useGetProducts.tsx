c2874bde3a20fd075bd0abd19fe126707b2fe1f9
khennyboy <abdullateefkehinde848@gmail.com>
2026-09-14 19:44:37 +0100

    delete backend

--- src/hooks/useGetProducts.tsx
+++ src/hooks/useGetProducts.tsx
@@ -9,7 +9,7 @@ import { useProductStore } from "../store/products-store";
 
 const fetchProducts = async (page: number, signal?: AbortSignal) => {
   const res = await fetch(
-    `${import.meta.env.VITE_API_URL}/products/?page=${page}`,
+    `${import.meta.env.BACKEND_URL}/products/?page=${page}`,
     {
       credentials: "include",
       signal,
